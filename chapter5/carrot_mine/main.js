52031810eef0e3c6d8100198c53b7e6030dc8f89
Jaye <jwha94@gmail.com>
2021-02-05 00:16:02 +0900

    실전10. 결과창추가, 클래스만들어야함

---
+++ chapter5/carrot_mine/main.js
@@ -0,0 +1,37 @@
+class carrotGame{
+  constructor(){
+    this.bug = 0;
+    this.carrot = 0;
+    this.counter = 0;
+    this.timer = 10;
+  }
+
+  // 랜덤좌표 구하는 함수
+  getCoordinates(){
+    
+  }
+
+  // 좌표값에 따라 그림을 뿌리는 함수
+  printImage(bugOrCarrot){
+    
+  }
+  
+  // 벌레누르면 youloose
+  showResult(){
+    
+  }
+  
+  // 당근 누르면 사라지고 카운터+1
+  getPoint(){
+
+  }
+  // 타이머
+
+}
+
+
+
+// 플레이버튼 누르면 게임실행(타이머,그림뿌리는함수 시작) 
+
+
+
