2f928373597a9158229f09a52b96b09c3f5d272c
JayeHa <66169832+JayeHa@users.noreply.github.com>
2021-02-17 11:36:44 +0900

    9.10 마지막 마무리 ⚡️

--- chapter5/carrot/src/game.js
+++ chapter5/carrot/src/game.js
@@ -1,4 +1,4 @@
-import Field from './field.js';
+import { Field, ItemType } from './field.js';
 import * as sound from './sound.js';
 
 export const Reason = Object.freeze({
@@ -85,14 +85,14 @@ class Game {
         if (!this.started) {
             return;
         }
-        if (item === 'carrot') {
+        if (item === ItemType.carrot) {
             this.score++;
             this.updateScoreBoard();
 
             if (this.score === this.carrotCount) {
                 this.stop(Reason.win);
             }
-        } else if (item === 'bug') {
+        } else if (item === ItemType.bug) {
             this.stop(Reason.lose);
         }
     };
