34475acc745259bf108679c312f123b1057280fb
JayeHa <66169832+JayeHa@users.noreply.github.com>
2021-02-17 11:30:26 +0900

    9.9 함수를 깔끔하게 🧽

--- chapter5/carrot/src/main.js
+++ chapter5/carrot/src/main.js
@@ -1,5 +1,6 @@
 'use strict'
 import PopUp from './popup.js';
+import * as sound from './sound.js';
 import { GameBuilder, Reason } from './game.js';
 
 const gameFinishBanner = new PopUp();
@@ -14,12 +15,15 @@ game.setGameStopListener((reason) => {
     switch (reason) {
         case Reason.cancel:
             message = 'Replay?'
+            sound.playAlert();
             break;
         case Reason.win:
             message = 'YOU WON!';
+            sound.playWin();
             break;
         case Reason.lose:
             message = 'YOU LOST:(';
+            sound.playBug();
             break;
         default:
             throw new Error('not valid reason');
@@ -30,3 +34,10 @@ game.setGameStopListener((reason) => {
 gameFinishBanner.setClickListener(() => {
     game.start();
 });
+
+// 이 게임 안에는 게임을 만들 수 있는 빌더와 각각의 DOM요소들을 받아와서
+// 게임 안에 있는 필드를 만들어서 게임을 start하고 stop하고
+// 아이템이 클릭되면 점수 계산을 해주는 것들이 포함되어져 있고요
+// 실제로 아이템들이 어떻게 배치되고 어떻게 클릭되는지는
+// 필드 안에서 정의되어져 있습니다.
+// 그리고 배너는 이렇게 팝업 안에 정의되어져 있고요
