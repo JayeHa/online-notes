6d8d2c57a2af572f62e79756d475913507b8d3a9
JayeHa <66169832+JayeHa@users.noreply.github.com>
2021-02-22 12:30:38 +0900

    2.5 기본 타입 정리 3 (unknown, any, void, never, object)

--- 1-tyeps/1-1-basic.ts
+++ 1-tyeps/1-1-basic.ts
@@ -15,20 +15,47 @@
     const boal:boolean = false;
 
     // undefined
-    let name: undefined; //똥
+    let name: undefined; //💩
     let age: number | undefined;
     age = undefined;
     age = 1;
-    
+
     function find(): number | undefined{
         return undefined;
     }
 
     // null
-    let person: null; //똥
+    let person: null; //💩
     let person2: string | null;
 
+    // unknown 💩
+    let notSure: unknown = 0;
+    notSure = 'he';
+    notSure = true;
 
+    // any  💩
+    let anything: any = 0;
+    anything = 'hello';
 
+    //void
+    function print(): void {
+        console.log('hello');
+        return;
+    }
+    let unusable: void = undefined; // 💩
+
+    //never
+    function throwError(message: string): never {
+        // message -> server (log)
+        throw new Error(message);
+        while (true) {}
+    }
+    let neverEnding: never; //💩
+
+    //  object
+    let obj: object; //💩
+    function acceptSomeObject(obj: object){}
+    acceptSomeObject({name:'ellie'});
+    acceptSomeObject({animal:'dog'});
 
 }
