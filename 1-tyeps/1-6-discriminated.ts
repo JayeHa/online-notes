e78b19a27836d796a0672db026fffe71fc3e4873
JayeHa <66169832+JayeHa@users.noreply.github.com>
2021-02-23 13:21:45 +0900

    2.11 필수 타입! Discriminated Union 🚀

--- 1-tyeps/1-6-discriminated.ts
+++ 1-tyeps/1-6-discriminated.ts
@@ -1,29 +1,20 @@
 {
-    /**
-     * Union Types: OR
-     */
-    type Direction = 'left' | 'right' | 'up' | 'down';
-    function move(direction: Direction){
-        console.log(direction);
-    }
-    move('down');
-
-    type TileSize = 8 | 16 | 32;
-    const tile: TileSize = 16;
-
     // function: login -> success, fail
     type SuccessState = {
+        result: 'success';
         response:{
             body: string;
         };
     };
     type FailState = {
+        result: 'fail'
         reason: string;
     };
     type LoginState = SuccessState | FailState;
 
-    function login(): LoginState {
+    function login1(): LoginState {
         return{
+            result: "success",
             response: {
                 body: 'logged in!',
             },
@@ -34,8 +25,10 @@
     // success -> 🎉 body
     // fail -> 😭 reason
 
-    function printLoginState(state:LoginState) {
-        if('response' in state ) {
+    function printLoginState1(state:LoginState) {
+        state.result //success or fail
+
+        if(state.result === 'success' ) {
             console.log(`🎉 ${state.response.body}`);           
         } else {
             console.log(`😭 ${state.reason}`);
