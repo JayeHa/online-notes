e0b6757e2555f168a2bfc26388e2d0454acf6400
JayeHa <66169832+JayeHa@users.noreply.github.com>
2021-03-03 16:09:30 +0900

    검색창

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,22 @@
 import React, { useEffect, useState } from 'react';
-import './app.css';
+import styles from './app.module.css';
+import Search_header from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
 
 function App() {
   const [videos, setVideos] = useState([]);
+  const search = query => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyDVutn91G5cSdoFsn-7BsJbSecurKpgZ6M`, requestOptions)
+      .then(response => response.json())
+      .then(result => result.items.map(item => ({...item, id: item.id.videoId})))
+      .then(items => setVideos(items))
+      .catch(error => console.log('error', error));
+  }
 
   useEffect(()=>{
     const requestOptions = {
@@ -18,7 +31,13 @@ function App() {
       .then(result => setVideos(result.items))
       .catch(error => console.log('error', error)); 
     }, []);
-  return <VideoList videos={videos}/>;
+    
+  return(
+  <div className={styles.app}>
+    <Search_header onSearch={search}/>
+    <VideoList videos={videos}/>
+  </div>
+  );
 }
 
 export default App;
