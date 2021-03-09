2375d723f952c4275cc650de9c2dfc6b89a5d31a
JayeHa <66169832+JayeHa@users.noreply.github.com>
2021-03-09 11:26:24 +0900

    JavaScript - display items

--- src/main.js
+++ src/main.js
@@ -6,11 +6,40 @@ function loadItems(){
     .then(json => json.items);
 }
 
+// Update the list with the given items
+function displayItems(items){
+    const container = document.querySelector('.items');
+    container.innerHTML = items.map(item => createHTMLString(item)).join('');
+
+    // 내가 한거...
+    // const items_container = document.querySelector(".items");
+    // items.map((item)=>{
+    //     let itemRow = document.createElement('li');
+    //     itemRow.setAttribute('class', 'item');
+    //     itemRow.innerHTML = `
+    //         <img src="${item.image}" class="item__thumbnail">
+    //         <span class="item__discription">${item.gender}, ${item.size}</span>
+    //     `
+    //     items_container.appendChild(itemRow);
+    // })
+}
+
+// Create HTML list item from the given data item
+function createHTMLString(item){
+    return `
+    <li class="item">
+        <img src="${item.image}" alt="${item.type}" class="item__thumbnail">
+        <span class="item__discription">${item.gender}, ${item.size}</span>
+    </li>
+    `;
+}
+
+
 // main
 loadItems()
     .then(items => {
         console.log(items);
-        // displayItems(items);
+        displayItems(items);
         // setEventListeners(items);
     })
     .catch(console.log())
