62d0c8a5ed57b05e15f640b2d73e7f199a3a4278
정원 <66169832+JayeHa@users.noreply.github.com>
2021-03-09 23:33:06 +0900

    8.8 검색 기능 프로처럼 리팩토링 💡

--- src/app.jsx
+++ src/app.jsx
@@ -3,33 +3,17 @@ import styles from './app.module.css';
 import Search_header from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
 
-function App() {
+function App({youtube}) {
   const [videos, setVideos] = useState([]);
   const search = query => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
+    youtube.search(query)
+    .then(videos => setVideos(videos))
     
-    fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyDVutn91G5cSdoFsn-7BsJbSecurKpgZ6M`, requestOptions)
-      .then(response => response.json())
-      .then(result => result.items.map(item => ({...item, id: item.id.videoId})))
-      .then(items => setVideos(items))
-      .catch(error => console.log('error', error));
   }
-
-  useEffect(()=>{
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    
-    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDVutn91G5cSdoFsn-7BsJbSecurKpgZ6M",
-    requestOptions
-    )
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error)); 
+  
+  useEffect(() => {
+    youtube.mostPopular()
+    .then(videos => setVideos(videos))
     }, []);
     
   return(
--- src/components/search_header/search_header.jsx
+++ src/components/search_header/search_header.jsx
@@ -25,7 +25,7 @@ const Search_header = ({onSearch}) => {
                 <h1 className={styles.title}>Youtube</h1>
             </div>
             <input ref={inputRef} className={styles.input} type="text" placeholder="search..." onKeyPress={onKeyPress}/>
-            <button className={styles.button} type="submit" onClick={onClick}><i class="fas fa-search"></i></button>
+            <button className={styles.button} type="submit" onClick={onClick}><i className="fas fa-search"></i></button>
         </header>
     )
 }
