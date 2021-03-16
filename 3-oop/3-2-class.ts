ad9d0e6a598bbb7b43038b9e86a39d48b3e7f265
JayeHa <66169832+JayeHa@users.noreply.github.com>
2021-03-16 18:19:45 +0900

    4.7 객체지향적으로 커피기계 만들기 💡 (static 사용)

--- 3-oop/3-2-class.ts
+++ 3-oop/3-2-class.ts
@@ -4,27 +4,37 @@
         hasMilk: boolean;
     };
 
-    class CoffeeMachine {
-        BEANS_GRAMM_PER_SHOT = 7;
-        coffeeBeans = 3 * this.BEANS_GRAMM_PER_SHOT;
-
-        makeCoffee(shots:number):CoffeeCup{
-            if(this.coffeeBeans<shots*this.BEANS_GRAMM_PER_SHOT){
-                throw new Error("not enough coffee beans!");
-            }
-
-            this.coffeeBeans -= shots*this.BEANS_GRAMM_PER_SHOT;
-
-            return {
-                shots,
-                hasMilk: false
-            }
+    class CoffeeMaker{
+        static BEANS_GRAMM_PER_SHOT: number = 7;   // class level
+        coffeeBeans: number = 0;                   // instance(object) level
+        
+        constructor(coffeeBeans: number) {
+            this.coffeeBeans = coffeeBeans;
         }
 
-}
+        static makeMachine(coffeeBeans: number): CoffeeMaker {
+            return new CoffeeMaker(coffeeBeans)
+        }
 
-const coffeeMachine = new CoffeeMachine;
-const coffee = coffeeMachine.makeCoffee(2);
-console.log(coffee);
-console.log(coffeeMachine.coffeeBeans);
+        makeCoffee(shots: number): CoffeeCup {
+            if(this.coffeeBeans < shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT){
+                throw new Error('Not enough coffee beans!');
+            }
+            this.coffeeBeans -= shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT;
+            return {
+                shots,
+                hasMilk: false,
+            };
+        }
+    }
+
+    const maker = new CoffeeMaker(32);
+    console.log(maker);
+    const maker2 = new CoffeeMaker(14);
+    console.log(maker2);
+    const maker3 = CoffeeMaker.makeMachine(3);
+    console.log(maker3);
+
+
+    
 }
