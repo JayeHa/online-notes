6855c29c9d2f20ba9b1357deb8d6ed37c1226a31
JayeHa <66169832+JayeHa@users.noreply.github.com>
2021-03-17 10:27:56 +0900

    4.11 Abstraction 추상화 설명

--- 3-oop/3-4-abstraction.ts
+++ 3-oop/3-4-abstraction.ts
@@ -4,12 +4,12 @@
         hasMilk: boolean;
     };
 
-    interface CoffeeMaker {
+    interface CoffeeMaker { // interface
         makeCoffee(shots: number): CoffeeCup;
 
     }
 
-    class CoffeeMachine implements CoffeeMaker{
+    class CoffeeMachine implements CoffeeMaker { //implements
         private static BEANS_GRAMM_PER_SHOT: number = 7;   // class level
         private coffeeBeans: number = 0;                   // instance(object) level
         
@@ -62,7 +62,8 @@
     maker.makeCoffee(2);
 
     const maker2: CoffeeMaker = CoffeeMachine.makeMachine(32);
-    maker2.fillCoffeeBeans(32);
+    maker2.fillCoffeeBeans(32); // 커피콩을 채우는 API는 CoffeeMaker라는 인터페이스에는 존재하지 않습니다. 그래서 인터페이스에 없는 이 함수를 사용할 수가 없어요.
+    // 그래서 이 인터페이스를 이용하면 내가 얼마만큼의 행동을 약속할 건지, 보장할건지, 허용할건지 결정할 수 있어요.
     maker2.makeCoffee(2);
 
 
