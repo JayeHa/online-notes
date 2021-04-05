7942e9999f402cdf5968236a4537da24ae4f9953
JayeHa <66169832+JayeHa@users.noreply.github.com>
2021-04-05 14:48:39 +0900

    MyMotion: 모달창 active
    버튼 눌렀을 때 모달창 띄우고 X버튼 누르면 모달창 없앰
    => 이벤트 위임을 썼는데, 웹에서는 문제없이 제대로 작동은 하지만 TS에서 Property 'classList' does not exist on type 'EventTarget' 오류메세지가 출력된당..

--- 12-project/build/main.js
+++ 12-project/build/main.js
@@ -1,6 +1,19 @@
 "use strict";
 console.log('hi');
 var menu = document.querySelector('.header__categories');
-menu.addEventListener("click", function () {
-    console.log('menuu');
+var modal = document.querySelector('.modal');
+menu.addEventListener("click", function (e) {
+    var target = e.target;
+    if (target.classList.contains('category__midea')) {
+        modal.classList.add('active');
+    }
+    else if (target.classList.contains('category__text')) {
+    }
+});
+modal.addEventListener("click", function (e) {
+    var target = e.target;
+    // console.log(target.className);
+    if (target.className && target.classList.contains('modal-close')) {
+        modal.classList.remove('active');
+    }
 });
--- 12-project/src/main.ts
+++ 12-project/src/main.ts
@@ -1,5 +1,21 @@
 console.log('hi')
 const menu = document.querySelector('.header__categories') as Element;
-menu.addEventListener("click", () => {
-   console.log('menuu');
+const modal = document.querySelector('.modal') as Element;
+
+menu.addEventListener("click", (e) => {
+   const target = e.target;
+   if(target.classList.contains('category__midea')){
+      modal.classList.add('active');  
+   } else if(target.classList.contains('category__text')){
+
+   }
+});
+
+modal.addEventListener("click", (e) => {
+   const target = e.target;
+   // console.log(target.className);
+   if(target.className && target.classList.contains('modal-close') ){
+      modal.classList.remove('active');
+   }
+   
 });
