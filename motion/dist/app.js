a1db6e1f274391c1b7e2f0b4ced897d367942f05
JayeHa <66169832+JayeHa@users.noreply.github.com>
2021-04-06 11:20:56 +0900

    update app.js

--- motion/dist/app.js
+++
@@ -1,8 +0,0 @@
-import { PageCompoent } from "./components/page.js";
-class App {
-    constructor(appRoot) {
-        this.page = new PageCompoent();
-        this.page.attachTo(appRoot);
-    }
-}
-new App(document.querySelector('.document'));
