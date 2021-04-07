0b1f2c2737ec44ee76c327ea8b544ebf7e8b79b6
JayeHa <66169832+JayeHa@users.noreply.github.com>
2021-04-07 10:45:39 +0900

    14.13 모든 섹션들 구현하기💡 (유튜브편)
    📌 iframe타입: HTMLIFrameElement
    📌 타입을 잘못 작성한 것이 있어서 여기에서 알려드려요:
      ✔HTMLHeadElement: HTML의 head 태그를 대표하는 요소
      ✔HTMLHeadingElement: h1과 같은 heading 태그를 대표하는 요소
    우리의 경우는 HTMLHeadingElement를 사용해야 합니다.

--- motion/src/app.ts
+++ motion/src/app.ts
@@ -2,6 +2,7 @@ import { TodoCompoenet } from './components/page/item/todo.js';
 import { ImageComponent } from "./components/page/item/image.js";
 import { NoteCompoenet } from "./components/page/item/note.js";
 import { PageCompoent } from "./components/page/page.js";
+import { VideoCompoent } from './components/page/item/video.js';
 
 class App {
    private readonly page: PageCompoent;
@@ -11,6 +12,9 @@ class App {
 
       const image = new ImageComponent('Image Title' , 'https://picsum.photos/600/300');
       image.attachTo(appRoot, 'beforeend');
+
+      const video = new VideoCompoent('Video title', 'https://youtu.be/F9NkdprdcdQ');
+      video.attachTo(appRoot, 'beforeend');
 
       const note = new NoteCompoenet('note Title', 'note body');
       note.attachTo(appRoot, 'beforeend');
--- motion/src/components/page/item/video.ts
+++ motion/src/components/page/item/video.ts
@@ -3,14 +3,16 @@ import { BaseComponent } from "../../component.js";
 export class VideoCompoent extends BaseComponent<HTMLElement>{
    constructor(title: string, url: string){
       super(`<section class="video">
-               <iframe class="video__holder" width="600" height="300"  title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-               <h2 class="video__title"></h2>
+               <div class="video__player"><iframe class="video__iframe"></iframe></div>
+               <h3 class="video__title"></h3>
             </section>`);
 
-      const videoElement = this.element.querySelector('.video__holder')! as HTMLVideoElement;
-      url = url.replace('youtu.be','www.youtube.com/embed');
-      videoElement.src = url;
-      const titleElement = this.element.querySelector('.video__title')! as HTMLParagraphElement;
+      const iframe = this.element.querySelector('.video__iframe')! as HTMLIFrameElement;
+      iframe.src = 'https://youtube.com/embed/F9NkdprdcdQ'; // url -> videoId -> embed
+      url;
+
+      const titleElement = this.element.querySelector('.video__title')! as HTMLHeadingElement;
       titleElement.textContent = title;
+
    }
 }
