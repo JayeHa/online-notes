35502577997d7bed1101747f29efa1cc6093533f
JayeHa <66169832+JayeHa@users.noreply.github.com>
2021-04-07 11:45:52 +0900

    14.17 페이지 아이템 컨테이너 만들기 💡
    📄 page.ts
    1. BaseComponent를 상속하는 클래스 📁PageItemComponent를 만듬
    2. 따라서 super안에 string전달
    3. 외부에서 어떤 아이템을 전달하냐에 따라서 section안에 추가해주는 🔨addChild라는 API만듬
     - attachTo라는 인터페이스를 통해서 컴포넌트를 우리가 전달한 것에 붙일 수 있도록 구현
    -> 어떤 child가 들어오는지는 알 수 없지만 Component의 인터페이스를 규격하는 아이이기 때문에 이 child에 있는 attchTo를 이용해서 child를 원하는 곳에 붙일 수 있음
    4. 📁PageComponent에 addchild를 이용해서 section Component(video, Note 등등)를 전달받아서 PageItemComponent로 감싸줌
    5. PageItemCompoenet와 PageComponent를 보면 공통적인 addChild가 있음 ->  다른 요소들을 함께 조립할 수 있는 Composable이라는 📝interface 생성
    
    📄 app.ts
    - 내부사항을 전혀 신경쓰지 않고 '야, 페이지야. 여기 이미지 만들었어 ImageComponent 좀 추가해'라고 하면 페이지가 알아서 자체적으로 PageItemComponent로 한 단계 감싼 다음에 만들어줌

--- motion/src/app.ts
+++ motion/src/app.ts
@@ -1,26 +1,27 @@
+import { Component } from './components/component';
 import { TodoCompoenet } from './components/page/item/todo.js';
 import { ImageComponent } from "./components/page/item/image.js";
 import { NoteCompoenet } from "./components/page/item/note.js";
-import { PageCompoent } from "./components/page/page.js";
+import { Composable, PageCompoent } from "./components/page/page.js";
 import { VideoCompoent } from './components/page/item/video.js';
 
 class App {
-   private readonly page: PageCompoent;
+   private readonly page: Component & Composable;
    constructor(appRoot: HTMLElement) {
-      this.page = new PageCompoent();
+      this.page = new PageCompoent(); // 우선은 생성자 안에서 PageComponent를 만들고 있는데요, 사실 안에서 다른 어떤 클래스를 만든다는 것은 위헙해요. 이런것들은 다 디펜던시 인젝션을 사용해서 외부로부터 주입받는 것이 더 확장가능하고 유닛테스트를 해 나가기에도 좋습니다.
       this.page.attachTo(appRoot);
 
       const image = new ImageComponent('Image Title' , 'https://picsum.photos/600/300');
-      image.attachTo(appRoot, 'beforeend');
+      this.page.addChild(image);
 
       const video = new VideoCompoent('Video title', 'https://youtu.be/4RiI-JZkeLQ');
-      video.attachTo(appRoot, 'beforeend');
+      this.page.addChild(video);
 
       const note = new NoteCompoenet('note Title', 'note body');
-      note.attachTo(appRoot, 'beforeend');
+      this.page.addChild(note);
 
       const todo = new TodoCompoenet('todo Title', 'todo body');
-      todo.attachTo(appRoot, 'beforeend');
+      this.page.addChild(todo);
    }
 }
 
