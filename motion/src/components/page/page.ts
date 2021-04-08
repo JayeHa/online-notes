e68684994fce6c84ecc54edd42087d4d4efea7d8
JayeHa <66169832+JayeHa@users.noreply.github.com>
2021-04-08 11:47:50 +0900

    14.19 아이템 삭제 기능 구현 하기 💡
    1. close버튼이 클릭되면 해당하는 pageItemComponent를 페이지에서 제거를 해주면 되겠죠.
    우선은 생성자 안에서 close라는 클래스를 가진 버튼을 가지고 오고, onclick에는 우리가 전달받은 콜백함수를 호출해주면 될 것같습니다.
     그래서 PageItemComponent안에 Close버튼이 클릭이 되면, 이 PageItemComponent 이 자체가 페이지에서 삭제가 되어져야겠죠. 하지만 이 자체만으로는 PageItemComponent는 자기가 어디에 속해있는지 전혀 모르기 때문에 그런일을 할 수가 없고요. 외부로부터 전달받은 콜백함수를 호출해주면 됩니다.
    
    2. 그래서 클래스 안에는 외부로부터 전달받은 콜백함수를 저장하고 있을 closeListener라는 변수를 만들고(없을수도 있으니까 Optional로)
    
    3. onCloseListener라는 타입은 아무런 인자도 전달받지 않고 리턴하지도 않아요. 그냥 "야 너 닫혔어"하고 알려주는 콜백함수입니다.
    
       4. 그래서 여기에 설정할 수 있는 외부함수도 만들면 되겠죠. setOnCloseListener는 우리가 들을 listener를 받아올 수 있고 아무것도 하지않는 void타입의 함수고요, this.closeListener를 전달받은 listener로 등록을 해주면 됩니다.
    
    5. 그러면 close가 클릭되면 this.closeListener가 있으면 this.closeListener를 호출해줄거예요.
    누군가가 등록을 해뒀다면 '야 close버튼이 클릭됐어'라고 알려만 주는거죠.
    
      6. PageCompoenet에서 PageItem을 생성해줬죠. 그래서 item을 만들고 item을 페이지에다가 추가해주었는데요.
    자, 이제는 item의 setOncloseListener의 우리의 콜백함수를 등록해줄거예요.
    PageItemComponent를  attach를 할 수 있다면 remove, 삭제도 할 수 있어야겠죠.  Component정의부분으로 가셔서요.
    
     7. 무언가를 추가할 수 있다면 삭제도 해줘야합니다. 그래서 removeFrom, parent컨테이너로부터 나 자신을 제거하는 API입니다.
    
       8. HTMLElement에는 removeChild라는 이런 API가 있어요. 이걸 이용해서 parent에서 나 자신을 제거할거예요.
       조금 더 안전하게 작성하고 싶다면 전달받은 parent요소가 내가 지금 들어있는 이 element의 parent인지 확인하면 좋겠죠.

--- motion/src/components/page/page.ts
+++ motion/src/components/page/page.ts
@@ -4,7 +4,9 @@ export interface Composable {
    addChild(child: Component): void;
 }
 
+type OnCloseListener = () => void; // 3. onCloseListener라는 타입은 아무런 인자도 전달받지 않고 리턴하지도 않아요. 그냥 "야 너 닫혔어"하고 알려주는 콜백함수입니다.
 class PageItemComponent extends BaseComponent<HTMLElement> implements Composable {
+   private closeListener?: OnCloseListener; // 2. 그래서 클래스 안에는 외부로부터 전달받은 콜백함수를 저장하고 있을 closeListener라는 변수를 만들고(없을수도 있으니까 Optional로)
    constructor() {
       super(`<li class="page-item">
                <section class="page-item__body"></section>
@@ -12,10 +14,27 @@ class PageItemComponent extends BaseComponent<HTMLElement> implements Composable
                   <button class="close">&times;</button>
                </div>
             </li>`);
+      // 1. close버튼이 클릭되면 해당하는 pageItemComponent를 페이지에서 제거를 해주면 되겠죠.
+      // 우선은 생성자 안에서 close라는 클래스를 가진 버튼을 가지고 오고,
+      // onclick에는 우리가 전달받은 콜백함수를 호출해주면 될 것같습니다.
+      // 그래서 PageItemComponent안에 Close버튼이 클릭이 되면, 이 PageItemComponent 이 자체가 페이지에서 삭제가 되어져야겠죠.
+      // 하지만 이 자체만으로는 PageItemComponent는 자기가 어디에 속해있는지 전혀 모르기 때문에 그런일을 할 수가 없고요.
+      // 외부로부터 전달받은 콜백함수를 호출해주면 됩니다.
+      const closeBtn = this.element.querySelector('.close')! as HTMLButtonElement;
+      closeBtn.onclick = () => {
+         this.closeListener && this.closeListener(); // 5. 그러면 close가 클릭되면 this.closeListener가 있으면 this.closeListener를 호출해줄거예요.
+         // 누군가가 등록을 해뒀다면 '야 close버튼이 클릭됐어'라고 알려만 주는거죠.
+      }
    }
    addChild(child: Component) {
       const container = this.element.querySelector('.page-item__body') ! as HTMLElement;
       child.attachTo(container);
+   }
+   // 4. 그래서 여기에 설정할 수 있는 외부함수도 만들면 되겠죠.
+   // setOnCloseListener는 우리가 들을 listener를 받아올 수 있고 아무것도 하지않는 void타입의 함수고요
+   // this.closeListener를 전달받은 listener로 등록을 해주면 됩니다.
+   setOnCloseListener(listener: OnCloseListener){
+      this.closeListener = listener;
    }
 }
 export class PageCompoent extends BaseComponent<HTMLUListElement> implements Composable {
@@ -27,6 +46,13 @@ export class PageCompoent extends BaseComponent<HTMLUListElement> implements Com
       const item = new PageItemComponent();
       item.addChild(section);
       item.attachTo(this.element, 'beforeend')
+      // 6. PageCompoenet에서 PageItem을 생성해줬죠. 그래서 item을 만들고 item을 페이지에다가 추가해주었는데요.
+      // 자, 이제는 item의 setOncloseListener의 우리의 콜백함수를 등록해줄거예요.
+      // PageItemComponent를  attach를 할 수 있다면 remove, 삭제도 할 수 있어야겠죠. 
+      // Component정의부분으로 가셔서요.
+      item.setOnCloseListener(() => {
+         item.removeFrom(this.element);
+      })
    }
 
 }
