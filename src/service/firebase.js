dde33e706d57d6c00ba05bcbac6aedc459b7139e
정원 <66169832+JayeHa@users.noreply.github.com>
2021-04-22 23:16:28 +0900

    10.2 로그인 화면 Firebase 추가 💡

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -1,5 +1,4 @@
 import firebase from 'firebase';
-
   const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -7,4 +6,5 @@ import firebase from 'firebase';
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   };
   // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  export default firebaseApp;
