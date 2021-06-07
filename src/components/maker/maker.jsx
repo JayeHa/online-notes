1b8c1d8fde482176f671d86b769c19fd7e0e79cb
정원 <66169832+JayeHa@users.noreply.github.com>
2021-06-07 22:40:56 +0900

    10.15 State 업데이트하기 💡(중요 내용 📔)

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -7,8 +7,8 @@ import Preview from '../preview/preview';
 import styles from './maker.module.css';
 
 const Maker = ({authService}) => {
-    const [cards, setCards] = useState([
-        {
+    const [cards, setCards] = useState({
+        '1':{
             id: '1',
             name: 'Eliie',
             company: 'Samsung',
@@ -19,7 +19,7 @@ const Maker = ({authService}) => {
             fileName: 'ellie',
             fileURL: null
         },
-        {
+        '2':{
             id: '2',
             name: 'Eliie2',
             company: 'Samsung',
@@ -30,7 +30,7 @@ const Maker = ({authService}) => {
             fileName: 'ellie',
             fileURL: 'ellie.png'
         },
-        {
+        '3':{
             id: '3',
             name: 'Eliie3',
             company: 'Samsung',
@@ -40,8 +40,9 @@ const Maker = ({authService}) => {
             message: 'go for it',
             fileName: 'ellie',
             fileURL: null
-        },
-    ]);
+        }
+    });
+
     const history = useHistory();
     const onLogout = () => {
         authService.logout();
@@ -55,16 +56,31 @@ const Maker = ({authService}) => {
         });
     });
 
-    const addCard = card => {
-        const updated = [...cards, card];
-        setCards(updated);
+    const createOrUpdateCard = card => {
+        setCards(cards => {
+            const updated = {...cards}; // setCards를 부를 때의 시점에서의 setCard상태 복사
+            updated[card.id] = card;
+            return updated;
+        }); 
+    };
+    
+    const deleteCard = card => {
+        setCards(cards => {
+            const updated = {...cards};
+            delete updated[card.id];
+            return updated
+        }); 
     }
 
     return(
         <section className={styles.maker}>
             <Header onLogout={onLogout}/>
             <div className={styles.container}>
-                <Editor cards={cards} addCard={addCard}/>
+                <Editor 
+                    cards={cards} 
+                    addCard={createOrUpdateCard} 
+                    updateCard={createOrUpdateCard} 
+                    deleteCard={deleteCard}/>
                 <Preview cards={cards}/>
             </div>
             <Footer />
