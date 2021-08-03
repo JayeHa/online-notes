76fc40b45f0f138a4459a3c3c1e5d864aa16e13c
정원 <66169832+JayeHa@users.noreply.github.com>
2021-08-03 23:52:44 +0900

    10.21 firebase

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -6,42 +6,10 @@ import Header from '../header/header';
 import Preview from '../preview/preview';
 import styles from './maker.module.css';
 
-const Maker = ({FileInput, authService}) => {
-    const [cards, setCards] = useState({
-        '1':{
-            id: '1',
-            name: 'Eliie',
-            company: 'Samsung',
-            theme: 'dark',
-            title: 'Software Engineer',
-            email: 'ellie@gmail.com',
-            message: 'go for it',
-            fileName: 'ellie',
-            fileURL: null
-        },
-        '2':{
-            id: '2',
-            name: 'Eliie2',
-            company: 'Samsung',
-            theme: 'colorful',
-            title: 'Software Engineer',
-            email: 'ellie@gmail.com',
-            message: 'go for it',
-            fileName: 'ellie',
-            fileURL: 'ellie.png'
-        },
-        '3':{
-            id: '3',
-            name: 'Eliie3',
-            company: 'Samsung',
-            theme: 'light',
-            title: 'Software Engineer',
-            email: 'ellie@gmail.com',
-            message: 'go for it',
-            fileName: 'ellie',
-            fileURL: null
-        }
-    });
+const Maker = ({FileInput, authService, cardRepository}) => {
+    const historyState = useHistory().state;
+    const [cards, setCards] = useState({});
+    const [userId, setUserId] = useState(historyState && historyState.id);
 
     const history = useHistory();
     const onLogout = () => {
@@ -50,27 +18,16 @@ const Maker = ({FileInput, authService}) => {
 
     useEffect(()=>{
         authService.onAuthChange(user => {
-            if(!user){
+            if(user){
+               setUserId(user.uid);
+               console.log(userId);
+            } else {
                 history.push('/');
             }
         });
     });
 
-    const createOrUpdateCard = card => {
-        setCards(cards => {
-            const updated = {...cards}; // setCards를 부를 때의 시점에서의 setCard상태 복사
-            updated[card.id] = card;
-            return updated;
-        }); 
-    };
-    
-    const deleteCard = card => {
-        setCards(cards => {
-            const updated = {...cards};
-            delete updated[card.id];
-            return updated
-        }); 
-    }
+
 
     return(
         <section className={styles.maker}>
