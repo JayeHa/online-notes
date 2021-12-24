505df087a88f3c6d85dc0a6e259f6d11aec9255d
EllyHa <jwha94@gmail.com>
2021-12-24 14:17:41 +0900

    4. Spread Syntax (자바스크립트 프로처럼 쓰는 팁 ✨ )

---
+++ pro-tips/spread-syntax-array.js
@@ -0,0 +1,12 @@
+// Spread Syntax - Array
+let fruits = ["🍉", "🍊", "🍌"];
+
+// fruits.push("🍓");
+fruits = [...fruits, "🍓"];
+
+// fruits.unshift("🍇");
+fruits = ["🍇", ...fruits];
+
+const fruits2 = ["🍈", "🍑", "🍍"];
+let combined = fruits.concat(fruits2);
+combined = [...fruits, "🍒", ...fruits2];
