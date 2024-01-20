7b20851f2bd9f99c952626acdcb088cf510481a6
정원 <66169832+JayeHa@users.noreply.github.com>
2024-01-21 03:07:57 +0900

    6.14 변수 캡슐화하기 (책 6.6)

--- DC-refactoring/ch6/6-6.js
+++ DC-refactoring/ch6/6-6.js
@@ -1,4 +1,28 @@
-let defaultOwner = { firstName: '마틴', lastName: '파울러' };
+// 1. 중첩 형태일 경우 깊은 복사 적용 X
+// let defaultOwner = { firstName: "마틴", lastName: "파울러" };
+
+// export function getDefaultOwner() {
+//   return { ...defaultOwner };
+
+// 2. 클래스를 이용하여 변경 여부 설정
+export class Person {
+  #firstName;
+  #lastName;
+  constructor(data) {
+    this.#firstName = data.firstName;
+    this.#lastName = data.lastName;
+  }
+
+  get firstName() {
+    return this.#firstName;
+  }
+
+  get lastName() {
+    return this.#lastName;
+  }
+}
+
+let defaultOwner = new Person({ firstName: "마틴", lastName: "파울러" });
 
 export function getDefaultOwner() {
   return defaultOwner;
