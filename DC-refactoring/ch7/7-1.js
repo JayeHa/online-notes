bdfe2fee23eee5490adaced7b6b67851889f763e
정원 <jwha94@gmail.com>
2024-01-24 03:35:41 +0900

    7.3 레코드 캡슐화하기 (책7.1)

--- DC-refactoring/ch7/7-1.js
+++ DC-refactoring/ch7/7-1.js
@@ -1,5 +1,33 @@
-const organization = { name: 'Acme Gooseberries', country: 'GB' };
+class Organization {
+  #name;
+  #country;
+  constructor({ name, country }) {
+    this.#name = name;
+    this.#country = country;
+  }
+  get name() {
+    return this.#name;
+  }
+  set name(value) {
+    this.#name = value;
+  }
+  get country() {
+    return this.#country;
+  }
+  set country(value) {
+    this.#country = value;
+  }
+  get rawData() {
+    // return { ...this.#data }; // 얕은복사, cloneDeep
+    return { name: this.name, country: this.country };
+  }
+}
 
-organization.name = 'Dream Coding';
+// const organization = new Organization("Acme Gooseberries", "GB");
+const organization = new Organization({
+  name: "Acme Gooseberries",
+  country: "GB",
+});
+organization.name = "Dream Coding";
 console.log(organization.name);
 console.log(organization.country);
