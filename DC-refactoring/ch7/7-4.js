ea7357a4dc105c038bbd0b69ed68d32943ac8943
정원 <jwha94@gmail.com>
2024-01-25 12:08:26 +0900

    7.6 임시 변수를 질의 함수로 바꾸기 (책 7.4)

--- DC-refactoring/ch7/7-4.js
+++ DC-refactoring/ch7/7-4.js
@@ -1,3 +1,8 @@
+/**
+ * 특정한 함수나 코드 블록에서 사용하고 있는 임시 변수(Temp)를 질의 함수(Query)로 대체하여 코드의 가독성과 재사용성을 높일 수 있습니다.
+ * 모든 임시 변수를 무조건적으로 getter로 변경해야 하는 것은 아니며, 필요한 경우에 한해 변경하는 것이 좋습니다.
+ */
+
 class Order {
   #quantity;
   #item;
@@ -6,10 +11,17 @@ class Order {
     this.#item = item;
   }
 
+  // 주문의 기본 가격을 계산하는 getter 함수
+  get basePrice() {
+    return this.#quantity * this.#item.price;
+  }
+
+  // 할인율을 계산하는 getter 함수
+  get discountFactor() {
+    return this.basePrice > 1000 ? 0.95 : 0.98;
+  }
+
   get price() {
-    const basePrice = this.#quantity * this.#item.price;
-    const discountFactor = 0.98;
-    if (basePrice > 1000) discountFactor -= 0.03;
-    return basePrice * discountFactor;
+    return this.basePrice * this.discountFactor;
   }
 }
