115514697bd0a724c88e632d4409304d30340dd5
정원 <jwha94@gmail.com>
2024-02-03 21:56:43 +0900

    8.7 문장 슬라이드하기 (책 8.6)

--- DC-refactoring/ch8/8-6.js
+++ DC-refactoring/ch8/8-6.js
@@ -1,18 +1,19 @@
+// 관련있는 코드들이 가까이 모여 있다면 코드를 이해할때나 리펙토링하기 더 쉬워진다.
+// 하나의 데이터 구조를 사용하는 문장들은 한데 모여있는 것이 좋다.
+// 변수를 선언한다면, 그 변수를 사용하는 곳과 밀접하게 가까이 하는게 좋다.
+
 // 예제 1
 const pricingPlan = retrievePricingPlan();
-const order = retreiveOrder();
-let charge;
 const chargePerUnit = pricingPlan.unit;
+const order = retrieveOrder();
+let charge;
 
 // 예제 2
 function someFunc() {
-  let result;
-  if (availableResources.length === 0) {
-    result = createResource();
-    allocatedResources.push(result);
-  } else {
-    result = availableResources.pop();
-    allocatedResources.push(result);
-  }
+  const result =
+    availableResources.length === 0
+      ? createResource()
+      : availableResources.pop();
+  allocatedResources.push(result);
   return result;
 }
