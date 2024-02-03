fccf517065d444193165a550c7cc1645feb86590
정원 <jwha94@gmail.com>
2024-02-03 21:42:08 +0900

    8.5 문장을 호출한 곳으로 옮기기 (책 8.4)

--- DC-refactoring/ch8/before/8-4.js
+++ DC-refactoring/ch8/before/8-4.js
@@ -2,7 +2,6 @@ function renderPerson(outStream, person) {
   outStream.write(`<p>${person.name}</p>\n`);
   renderPhoto(outStream, person.photo);
   emitPhotoData(outStream, person.photo);
-  outStream.write(`<p>location: ${photo.location}</p>\n`);
 }
 
 function listRecentPhotos(outStream, photos) {
@@ -11,7 +10,6 @@ function listRecentPhotos(outStream, photos) {
     .forEach((p) => {
       outStream.write("<div>\n");
       emitPhotoData(outStream, p);
-      outStream.write(`<p>위치: ${p.location}</p>\n`);
       outStream.write("</div>\n");
     });
 }
@@ -19,6 +17,7 @@ function listRecentPhotos(outStream, photos) {
 function emitPhotoData(outStream, photo) {
   outStream.write(`<p>title: ${photo.title}</p>\n`);
   outStream.write(`<p>date: ${photo.date.toDateString()}</p>\n`);
+  outStream.write(`<p>location: ${photo.location}</p>\n`);
 }
 
 function renderPhoto(outStream, aPhoto) {
