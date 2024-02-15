2ec558361dc69737aaf9a379cbaec19077e5a4ac
정원 <jwha94@gmail.com>
2024-02-15 22:49:36 +0900

    9.3 필드 이름 바꾸기 (책 9.2)

--- DC-refactoring/ch9/9-2.js
+++ DC-refactoring/ch9/9-2.js
@@ -1,12 +1,16 @@
+/**
+ * 아래와 같이 내부 이름은 그대로 유지하고 코드베이스에서 사용할 때는 title로 변경할 수 있다.
+ * 요지) 클래스/객체 안에 있는 필드는 이름이 너무나 중요하기 때문에 의도를 나타낼 수 있는 이름으로 바꾸어 나가야 한다.
+ */
 class Organization {
   constructor(data) {
-    this._name = data.name;
+    this._name = data.title;
     this._country = data.country;
   }
-  get name() {
+  get title() {
     return this._name;
   }
-  set name(value) {
+  set title(value) {
     this._name = value;
   }
   get country() {
@@ -17,6 +21,6 @@ class Organization {
   }
 }
 const organization = new Organization({
-  name: '드림코딩',
-  country: '대한민국',
+  title: "드림코딩",
+  country: "대한민국",
 });
