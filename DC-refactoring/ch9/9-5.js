7a7d35cd46fe412d072e6fda3f72d6b7e798f5df
정원 <jwha94@gmail.com>
2024-03-18 16:05:57 +0900

    9.6 값을 참조로 바꾸기 (책 9.5)

--- DC-refactoring/ch9/9-5.js
+++ DC-refactoring/ch9/9-5.js
@@ -1,7 +1,23 @@
+/**
+ * 불변성은 좋다
+ * -> 프로그램의 안정성을 높여주니까!
+ *
+ * But,
+ * 내 변경사항이 다른 곳에 즉각적으로 변경이 되어야 하는 경우(ex. 이름)라면, 가변성인 참조를 사용해야 한다.
+ * 그리고 이 참조를 사용할 때, 고유한 아이디 별로 하나의 인스턴스를 보장하고 싶다면 레파지토리 패턴을 사용하는 것이 좋다.
+ */
+
+const customerRepository = new CustomerRepository();
+
+const order = new Order(
+  data.number,
+  customerRepository.registerCustomer(data.customerId)
+);
+
 class Order {
-  constructor(data) {
-    this._number = data.number;
-    this._customer = new Customer(data.customerId);
+  constructor(number, customer) {
+    this._number = number;
+    this._customer = customer;
   }
 
   get customer() {
@@ -10,11 +26,31 @@ class Order {
 }
 
 class Customer {
-  constructor(id) {
+  constructor(id, name) {
     this._id = id;
+    this._name = name;
   }
 
   get id() {
     return this._id;
+  }
+}
+
+class CustomerRepository {
+  #customers;
+
+  constructor() {
+    this.#customers = new Map();
+  }
+
+  registerCustomer(id) {
+    if (!this.#customers.has(id)) {
+      this.#customers.set(id, new Customer(id));
+    }
+    return findCustomer(id);
+  }
+
+  findCustomer(id) {
+    return this.#customers.get(id);
   }
 }
