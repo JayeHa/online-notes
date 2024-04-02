00f3e8676803d41ed347c8dcf51ca24e8baff653
정원 <jwha94@gmail.com>
2024-04-02 22:20:10 +0900

    10.2 조건문 분해하기 (책 10.1)

--- DC-refactoring/ch10/10-1.js
+++ DC-refactoring/ch10/10-1.js
@@ -1,7 +1,16 @@
 function calculateCharge(date, quantity, plan) {
-  let charge = 0;
-  if (!date.isBefore(plan.summerStart) && !date.isAfter(plan.summerEnd))
-    charge = quantity * plan.summerRate;
-  else charge = quantity * plan.regularRate + plan.regularServiceCharge;
-  return charge;
+  return isSummer() ? summerCharge() : regularCharge();
+
+  // 아래 중첩함수가 외부에서 재사용이 필요하다면 외부로 추출 or 클래스화
+  function isSummer() {
+    return !date.isBefore(plan.summerStart) && !date.isAfter(plan.summerEnd);
+  }
+
+  function summerCharge() {
+    return quantity * plan.summerRate;
+  }
+
+  function regularCharge() {
+    return quantity * plan.regularRate + plan.regularServiceCharge;
+  }
 }
