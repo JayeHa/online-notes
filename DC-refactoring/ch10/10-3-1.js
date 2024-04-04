549b38df899e87a255d5a77017d1280380459f73
정원 <jwha94@gmail.com>
2024-04-04 19:34:03 +0900

    10.4 중첩 조건문을 보호 구문으로 바꾸기 (책 10.3)

--- DC-refactoring/ch10/10-3-1.js
+++ DC-refactoring/ch10/10-3-1.js
@@ -1,22 +1,23 @@
+// 보호 구문: 한 쪽만 정상이라면 비정상 조건을 if에서 검사 ✨
+// "이건 함수의 핵심이 아니다. 이 일이 일어나면 무언가 조치를 취한 후 함수에서 빠져나온다"라는 의도를 부각시킴
+
+// cf. if-else절: 두 경로 모두 정상 동작인 경우 사용
 export function payAmount(employee) {
-  let result;
   if (employee.isSeparated) {
-    result = { amount: 0, reasonCode: 'SEP' };
-  } else {
-    if (employee.isRetired) {
-      result = { amount: 0, reasonCode: 'RET' };
-    } else {
-      // lorem.ipsum(dolor.sitAmet);
-      // consectetur(adipiscing).elit();
-      // sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
-      // ut.enim.ad(minim.veniam);
-      result = someFinalComputation();
-      return result;
-    }
+    return { amount: 0, reasonCode: "SEP" };
   }
-  return result;
+
+  if (employee.isRetired) {
+    return { amount: 0, reasonCode: "RET" };
+  }
+
+  // lorem.ipsum(dolor.sitAmet);
+  // consectetur(adipiscing).elit();
+  // sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
+  // ut.enim.ad(minim.veniam);
+  return someFinalComputation();
 }
 
 function someFinalComputation() {
-  return { amount: 999, reasonCode: 'UNICORN' };
+  return { amount: 999, reasonCode: "UNICORN" };
 }
