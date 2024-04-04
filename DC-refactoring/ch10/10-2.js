e631a249d32576e8fa7e56b4cfd869eb3368597b
정원 <jwha94@gmail.com>
2024-04-04 12:40:51 +0900

    10.3 조건식 통합하기 (책 10.2)

--- DC-refactoring/ch10/10-2.js
+++ DC-refactoring/ch10/10-2.js
@@ -1,6 +1,16 @@
 function disabilityAmount(employee) {
-  if (employee.seniority < 2) return 0;
-  if (employee.monthsDisabled > 12) return 0;
-  if (employee.isPartTime) return 0;
+  if (isNotEligibleForDisability(employee)) {
+    return 0;
+  }
   return 1;
+}
+
+// 동일한 동작을 수행하는 다양한 조건들이 있다면,
+// 그 조건들을 하나의 조건문으로 뭉친 다음에 그에 알맞는 이름(의도)를 정해준다.
+function isNotEligibleForDisability(employee) {
+  return (
+    employee.seniority < 2 ||
+    employee.monthsDisabled > 12 ||
+    employee.isPartTime
+  );
 }
