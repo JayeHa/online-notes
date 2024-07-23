4d13eedebad263ab85144ce2572b78df24c9909b
정원 <jwha94@gmail.com>
2024-07-23 20:47:38 +0900

    10.6 조건부 로직을 다형성으로 바꾸기2 (책 10.4)

--- DC-refactoring/ch10/10-4-2.js
+++ DC-refactoring/ch10/10-4-2.js
@@ -1,58 +1,84 @@
+/**
+ * 여러 다른 타입이 있어서 switch로 타입을 하나하나 검사한다면, 다형성을 검사할 수는 없을까 생각해볼 수 있다.
+ * but 어느정도 설계를 해나가다보면, 상속을 너무 남용하는 것보다 위임(컴포지션)을 사용할 때가 더 나은 경우가 있음
+ * -> 이런 판단은 개발 경험을 쌓아나가면서 up
+ */
+
 export function rating(voyage, history) {
-  // 투자 등급
-  const vpf = voyageProfitFactor(voyage, history);
-  const vr = voyageRisk(voyage);
-  const chr = captainHistoryRisk(voyage, history);
-  if (vpf * 3 > vr + chr * 2) return 'A';
-  else return 'B';
+  return voyage.zone === "china" && history.some((v) => "china" === v.zone)
+    ? new ExperiencedChinaRating(voyage.history).value
+    : new Rating(voyage, history).value;
 }
 
-function voyageRisk(voyage) {
-  // 항해 경로 위험요소
-  let result = 1;
-  if (voyage.length > 4) result += 2;
-  if (voyage.length > 8) result += voyage.length - 8;
-  if (['china', 'east-indies'].includes(voyage.zone)) result += 4;
-  return Math.max(result, 0);
-}
-
-function captainHistoryRisk(voyage, history) {
-  // 선장의 항해 이력 위험 요소
-  let result = 1;
-  if (history.length < 5) result += 4;
-  result += history.filter((v) => v.profit < 0).length;
-  if (voyage.zone === 'china' && hasChina(history)) result -= 2;
-  return Math.max(result, 0);
-}
-
-function hasChina(history) {
-  // 중국을 경유하는가?
-  return history.some((v) => 'china' === v.zone);
-}
-
-function voyageProfitFactor(voyage, history) {
-  // 수익 요인
-  let result = 2;
-  if (voyage.zone === 'china') result += 1;
-  if (voyage.zone === 'east-indies') result += 1;
-  if (voyage.zone === 'china' && hasChina(history)) {
-    result += 3;
-    if (history.length > 10) result += 1;
-    if (voyage.length > 12) result += 1;
-    if (voyage.length > 18) result -= 1;
-  } else {
-    if (history.length > 8) result += 1;
-    if (voyage.length > 14) result -= 1;
+class Rating {
+  constructor(voyage, history) {
+    this.voyage = voyage;
+    this.history = history;
   }
-  return result;
+
+  get value() {
+    // 투자 등급
+    const profit = this.voyageProfitFactor;
+    const risk = this.voyageRisk;
+    const historyRisk = this.captainHistoryRisk;
+    return profit * 3 > risk + historyRisk * 2 ? "A" : "B";
+  }
+
+  get voyageRisk() {
+    // 항해 경로 위험요소
+    let result = 1;
+    if (this.voyage.length > 4) result += 2;
+    if (this.voyage.length > 8) result += this.voyage.length - 8;
+    if (["china", "east-indies"].includes(this.voyage.zone)) result += 4;
+    return Math.max(result, 0);
+  }
+
+  get captainHistoryRisk() {
+    // 선장의 항해 이력 위험 요소
+    let result = 1;
+    if (this.history.length < 5) result += 4;
+    result += this.history.filter((v) => v.profit < 0).length;
+    return Math.max(result, 0);
+  }
+
+  get voyageProfitFactor() {
+    // 수익 요인
+    let result = 2;
+    if (this.voyage.zone === "china") result += 1;
+    if (this.voyage.zone === "east-indies") result += 1;
+    result += this.voyageHistoryAndLengthFactor;
+    return result;
+  }
+
+  get voyageHistoryAndLengthFactor() {
+    let result = 0;
+    if (this.history.length > 8) result += 1;
+    if (this.voyage.length > 14) result -= 1;
+    return result;
+  }
 }
 
-const voyage = { zone: 'west-indies', length: 10 };
+class ExperiencedChinaRating extends Rating {
+  get captainHistoryRisk() {
+    const result = this.super.captainHistoryRisk - 2;
+    return Math.max(result, 0);
+  }
+
+  get voyageHistoryAndLengthFactor() {
+    let result = 3;
+    if (this.history.length > 10) result += 1;
+    if (this.voyage.length > 12) result += 1;
+    if (this.voyage.length > 18) result -= 1;
+    return result;
+  }
+}
+
+const voyage = { zone: "west-indies", length: 10 };
 const history = [
-  { zone: 'east-indies', profit: 5 },
-  { zone: 'west-indies', profit: 15 },
-  { zone: 'china', profit: -2 },
-  { zone: 'west-africa', profit: 7 },
+  { zone: "east-indies", profit: 5 },
+  { zone: "west-indies", profit: 15 },
+  { zone: "china", profit: -2 },
+  { zone: "west-africa", profit: 7 },
 ];
 
 const rate = rating(voyage, history);
