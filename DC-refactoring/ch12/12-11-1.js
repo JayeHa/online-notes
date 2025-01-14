c739dd1a4ebcce3d44ebc55874f6eb08a0efeec8
정원 <jwha94@gmail.com>
2025-01-14 22:05:57 +0900

    12.14 슈퍼클래스를 위임으로 바꾸기 (책 12.11)

--- DC-refactoring/ch12/12-11-1.js
+++ DC-refactoring/ch12/12-11-1.js
@@ -1,3 +1,19 @@
 class List {}
 
-class Stack extends List {}
+class Stack {
+  constructor() {
+    this.storage = new List();
+  }
+
+  pop() {}
+
+  push() {}
+}
+
+/**
+ * before:
+ * 노출하고 싶지 않는 것들도 다 상속받게 됨
+ *
+ * after:
+ * 내부에서 리스트를 사용 -> 원하는 동작들만 외부로 노출
+ */
