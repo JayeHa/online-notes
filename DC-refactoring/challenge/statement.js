8d9a62accab84becda1f284dd68fbe6c8353f01b
정원 <jwha94@gmail.com>
2025-01-21 21:08:39 +0900

    반복문 쪼개기(totalAmount)

--- DC-refactoring/challenge/statement.js
+++ DC-refactoring/challenge/statement.js
@@ -1,15 +1,13 @@
 export function statement(invoice, plays) {
-  let totalAmount = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
   for (let perf of invoice.performances) {
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${
       perf.audience
     }석)\n`;
-    totalAmount += amountFor(perf);
   }
 
-  result += `총액: ${usd(totalAmount / 100)}\n`;
+  result += `총액: ${usd(totalAmount() / 100)}\n`;
   result += `적립 포인트: ${totalCredits()}점\n`;
   return result;
 
@@ -50,6 +48,10 @@ export function statement(invoice, plays) {
     }
 
     return result;
+  }
+
+  function totalAmount() {
+    return invoice.performances.reduce((sum, p) => (sum += amountFor(p)), 0);
   }
 
   function totalCredits() {
