9864efed0c1ee9962c1b81c8cf90906569d6a4d5
정원 <jwha94@gmail.com>
2025-01-21 20:52:03 +0900

    중첩함수로 끌어올리기, 질의함수로 변경

--- DC-refactoring/challenge/statement.js
+++ DC-refactoring/challenge/statement.js
@@ -4,19 +4,17 @@ export function statement(invoice, plays) {
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
   for (let perf of invoice.performances) {
-    let thisAmount = amountFor(playFor(performance), perf);
-
     // 포인트를 적립한다.
     volumeCredits += Math.max(perf.audience - 30, 0);
     // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ("comedy" === playFor(performance).type)
+    if ("comedy" === playFor(perf).type)
       volumeCredits += Math.floor(perf.audience / 5);
 
     // 청구 내역을 출력한다.
-    result += `  ${playFor(performance).name}: ${format(thisAmount / 100)} (${
+    result += `  ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
       perf.audience
     }석)\n`;
-    totalAmount += thisAmount;
+    totalAmount += amountFor(perf);
   }
   result += `총액: ${format(totalAmount / 100)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
@@ -25,30 +23,30 @@ export function statement(invoice, plays) {
   function playFor(performances) {
     return plays[performances.playID];
   }
-}
 
-function amountFor(play, performance) {
-  let result = 0;
+  function amountFor(performance) {
+    let result = 0;
 
-  switch (play.type) {
-    case "tragedy": // 비극
-      result = 40000;
-      if (performance.audience > 30) {
-        result += 1000 * (performance.audience - 30);
-      }
-      break;
-    case "comedy": // 희극
-      result = 30000;
-      if (performance.audience > 20) {
-        result += 10000 + 500 * (performance.audience - 20);
-      }
-      result += 300 * performance.audience;
-      break;
-    default:
-      throw new Error(`알 수 없는 장르: ${play.type}`);
+    switch (playFor(performance).type) {
+      case "tragedy": // 비극
+        result = 40000;
+        if (performance.audience > 30) {
+          result += 1000 * (performance.audience - 30);
+        }
+        break;
+      case "comedy": // 희극
+        result = 30000;
+        if (performance.audience > 20) {
+          result += 10000 + 500 * (performance.audience - 20);
+        }
+        result += 300 * performance.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${playFor(performance).type}`);
+    }
+
+    return result;
   }
-
-  return result;
 }
 
 function format(number) {
